81281726666a3f9193764e369d24f42cfb1d9caa
Stevensd99 <stevensd@countryday.net>
2017-03-14 18:42:13 -0400

    ArrayList version 1
    Added the ability to have an unlimited contact list via an arrayList, and add elements via that.
    
    -Derek Stevens

--- AddressBook.java
+++ AddressBook.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-    private Contact[] friends; // An array of Contacts - each stores info for one friend
+    private ArrayList<String> friends = new ArrayList<String>; // An array of Contacts - each stores info for one friend
     private int numfriends; // Number of friends currently in AddressBook
 
     // Create an empty AddressBook
     public AddressBook() {
-		friends = new Contact[10];
 		numfriends = 0;
+	    	for(Adressbook cc: friends){
+			friends.add(cc)
+		}
+    }	
+    public void addItem(String name){
+	    friends.add(name)
+	    
     }
+}
